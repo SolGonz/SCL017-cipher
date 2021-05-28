256c88738c55acf372756a170da335bdd806179a
Solange Gonzalez <solgonzalezaraya@gmail.com>
2021-05-28 11:58:16 -0400

    proyecto terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
+
 const cipher = {
-  // ...
+  encode: (n, string) =>{
+    let newString = "";
+    for (let i=0; i < string.length; i++){
+            let ascNum = string.charCodeAt(i);
+            if(ascNum >= 65 && ascNum <= 90){
+            ascNum = (((ascNum-65)+n)%26)+65;
+        }
+        else if (ascNum >=97 && ascNum <=122){
+            ascNum = (((ascNum-97)+n)%26)+97;
+        }
+        newString+=String.fromCharCode(ascNum);
+    }
+
+    return newString;
+  },
+decode: (nDec, stringDec) =>{
+    let newStringDec = "";
+    for (let i=0; i < stringDec.length; i++){
+            let ascNumDec = stringDec.charCodeAt(i);
+            if(ascNumDec >= 65 && ascNumDec <= 90){
+            ascNumDec = (((ascNumDec-65)-nDec)%26)+65;
+        }
+        else if (ascNumDec>=97 && ascNumDec <=122){
+            ascNumDec = (((ascNumDec-97)-nDec)%26)+97;
+        }
+        newStringDec+=String.fromCharCode(ascNumDec);
+    }
+    return newStringDec;
+  }
 };
 
-export default cipher;
+// export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,87 @@
-import cipher from './cipher.js';
+function pantallaPsyduck(){
+    document.getElementById("primera-vista-psy").style.display = "block";
+    document.getElementById("vista-cero").style.display = "none";
+}
+function enviarMensaje(){
+    document.getElementById("primera-vista-psy").style.display = "none";
+    document.getElementById("segunda-vista-psy").style.display = "block";
+    mensajeEnviado = document.getElementById("mensaje").value;
+}
+  function enviarMensaje(){
+    document.getElementById("primera-vista-psy").style.display = "none";
+    document.getElementById("segunda-vista-psy").style.display = "block";
+    mensajeEnviado = document.getElementById("mensaje").value;
+  }
+  
+  function traducirMensaje(){
+    document.getElementById("segunda-vista-psy").style.display = "none";
+    document.getElementById("tercera-vista-psy").style.display = "block";
+    document.querySelector(".mnsj-cifrado").innerHTML =  cipher.encode(3, mensajeEnviado);
+  }
+  
+  function mensajeOriginal(){
+    document.getElementById("tercera-vista-psy").style.display = "none";
+    document.getElementById("cuarta-vista-psy").style.display = "block";
+    document.querySelector(".mnsj-original").innerHTML =  mensajeEnviado;
+  }
+  function inicioPsy(){
+    document.getElementById("cuarta-vista-psy").style.display = "none";
+    document.getElementById("vista-cero").style.display = "block";
+  }
+  
+  function pantallaPikachu(){
+    document.getElementById("primera-vista-pik").style.display = "block";
+    document.getElementById("vista-cero").style.display = "none";
+  }
+  
+  function enviarMensajePik(){
+    document.getElementById("segunda-vista-pik").style.display = "block";
+    document.getElementById("primera-vista-pik").style.display = "none";
+    mensajeEnviadoPik = document.getElementById("mensaje-pik").value;
+  }
+  
+  function traducirMensajePik(){
+    document.getElementById("tercera-vista-pik").style.display = "block";
+    document.getElementById("segunda-vista-pik").style.display = "none";
+    document.querySelector(".mnsj-cifrado-pik").innerHTML =  cipher.encode(4, mensajeEnviadoPik);
+  }
+  
+  function mensajeOriginalPik(){
+    document.getElementById("cuarta-vista-pik").style.display = "block";
+    document.getElementById("tercera-vista-pik").style.display = "none";
+    document.querySelector(".mnsj-original-pik").innerHTML =  cipher.decode(4,cipher.encode(4, mensajeEnviadoPik));
+  
+  }
+  
+  function inicioPik(){
+    document.getElementById("cuarta-vista-pik").style.display = "none";
+    document.getElementById("vista-cero").style.display = "block";
+  }
+  function pantallaEevee(){
+    document.getElementById("primera-vista-eev").style.display = "block";
+    document.getElementById("vista-cero").style.display = "none";
+  }
+  function enviarMensajeEev(){
+    document.getElementById("segunda-vista-eev").style.display = "block";
+    document.getElementById("primera-vista-eev").style.display = "none";
+    mensajeEnviadoEev = document.getElementById("mensaje-eev").value;
+  }
+  
+  function traducirMensajeEev(){
+    document.getElementById("tercera-vista-eev").style.display = "block";
+    document.getElementById("segunda-vista-eev").style.display = "none";
+    document.querySelector(".mnsj-cifrado-eev").innerHTML =  cipher.encode(5, mensajeEnviadoEev);
+  }
+  
+  function mensajeOriginalEev(){
+    document.getElementById("cuarta-vista-eev").style.display = "block";
+    document.getElementById("tercera-vista-eev").style.display = "none";
+    document.querySelector(".mnsj-original-eev").innerHTML = mensajeEnviadoEev;
+  }
+  
+  function inicioEev(){
+    document.getElementById("cuarta-vista-eev").style.display = "none";
+    document.getElementById("vista-cero").style.display = "block";
+  }
 
-console.log(cipher);
+// import cipher from './cipher.js';
